e9279b2737835065012fd20c072cef9e80277d9d
JamesLaviron <victor@gerfaud.fr>
2020-09-08 22:25:53 +0200

    Refacto authentication

--- functions/update.js
+++ functions/update.js
@@ -1,3 +1,11 @@
+import { authenticate } from './utils/authentication'
+
+/**
+ * Webhook main method
+ */
 export async function updateKanban() {
     console.log(`Update kanban!`)
+
+    var accessToken = await authenticate()
+    console.log(accessToken)
 }
