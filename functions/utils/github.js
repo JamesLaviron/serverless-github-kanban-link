0ce3da48ff721e59787b90c49368037b9761bee9
JamesLaviron <victor@gerfaud.fr>
2020-10-14 13:27:51 +0200

    Check for push event on webhook init

--- functions/utils/github.js
+++ functions/utils/github.js
@@ -1,15 +1,17 @@
 /**
  * Validate that the given body is as the required given event in it
  *
- * @param {Object} body          JSON body from GitHub event
- * @param {string} requiredEvent Required enabled event for the webhook
+ * @param {Object} body           JSON body from GitHub event
+ * @param {array}  requiredEvents Required enabled event for the webhook
  *
  * @returns {string}
  */
-export function validateWebhook(body, requiredEvent = `pull_request`) {
-  if (!body.hook.events.includes(requiredEvent)) {
-    throw new Error(`This webhook needs the "${requiredEvent}" event. Please tick it.`)
-  }
+export function validateWebhook(body, requiredEvents = [`pull_request`, `push`]) {
+  requiredEvents.forEach((event) => {
+    if (!body.hook.events.includes(event)) {
+      throw new Error(`This webhook needs the "${event}" event. Please tick it.`)
+    }
+  })
 
   if ((`organization` in body)) {
     return `Hello ${body.sender.login}, the webhook is now enabled for the organization ${body.organization.login}, enjoy!`
